d0ef97354c993d133f168ae6a9f116dd26d6740d
veereshn696-alt <veereshn696@gmail.com>
2025-10-22 13:10:54 +0530

    calculated si

---
+++ lab1.py
@@ -0,0 +1,11 @@
+# simple_interest.py
+
+# Principal, Rate and Time
+P = 10000   # Principal amount
+R = 5       # Rate of Interest (%)
+T = 3       # Time in years
+
+# Formula for Simple Interest
+SI = (P * R * T) / 100
+
+print("Simple Interest: ₹", SI)
